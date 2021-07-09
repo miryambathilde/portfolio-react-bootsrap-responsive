238958a1573fa403b1ff06dd322e63cda4cdf332
Miryam Bathilde <mbathilde@gmail.com>
2021-07-09 18:51:22 +0200

    template project finished

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {faFacebook} from '@fortawesome/free-brands-svg-icons';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import {faYoutube} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {Link} from 'react-router-dom';
 
@@ -11,7 +10,7 @@ function HomePage() {
             <header className="hero">
                 <h1 className="hero-text">
                     Hi, I am 
-                    <span> Lorem Ipsum.</span>
+                    <span> Jane Done</span>
                 </h1>
                 <p className="h-sub-text">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -25,9 +24,6 @@ function HomePage() {
                     </Link>
                     <Link to="test" className="icon-holder">
                         <FontAwesomeIcon icon={faGithub} className="icon gh" />
-                    </Link>
-                    <Link to="test" className="icon-holder">
-                        <FontAwesomeIcon icon={faYoutube} className="icon yt"/>
                     </Link>
                 </div>
             </header>
